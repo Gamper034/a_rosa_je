da9fe6f3a7c8fc7edba8bedc2b8d9124a1849d92
Anthony Diaz <ad-dev@outlook.fr>
2024-01-15 12:22:32 +0100

    Add: url localhost for api

--- lib/services/api/data_api.dart
+++ lib/services/api/data_api.dart
@@ -1,7 +1,23 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:flutter/foundation.dart' show kIsWeb, TargetPlatform;
+import 'package:flutter/foundation.dart' show defaultTargetPlatform;
 
 class DataApi {
+  String getHost() {
+    if (kIsWeb) {
+      return 'localhost';
+    }
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.android:
+        return '10.0.2.2';
+      case TargetPlatform.iOS:
+        return 'localhost';
+      default:
+        return 'localhost';
+    }
+  }
+
   Future<Map<String, dynamic>> registerUser(String role, String firstname,
       String lastname, String email, String password) async {
     try {
@@ -14,7 +30,7 @@ class DataApi {
       };
 
       final response = await http.post(
-        Uri.parse('http://localhost:2000/api/user/register'),
+        Uri.parse('http://${getHost()}:2000/api/user/register'),
         headers: <String, String>{
           'Content-Type': 'application/json; charset=UTF-8',
         },
@@ -43,7 +59,7 @@ class DataApi {
       };
 
       final response = await http.post(
-        Uri.parse('http://localhost:2000/api/user/register'),
+        Uri.parse('http://${getHost()}:2000/api/user/register'),
         headers: <String, String>{
           'Content-Type': 'application/json; charset=UTF-8',
         },
