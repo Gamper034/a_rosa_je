6a12998274e583e628974bb8bc26c760aec4c5d5
Anthony Diaz <ad-dev@outlook.fr>
2024-01-28 11:42:25 +0100

    Add: buttons

--- lib/pages/my_guards/my_guards.dart
+++ lib/pages/my_guards/my_guards.dart
@@ -1,3 +1,6 @@
+import 'package:a_rosa_je/theme/color.dart';
+import 'package:a_rosa_je/theme/textstyle.dart';
+import 'package:a_rosa_je/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
 class MyGuards extends StatefulWidget {
@@ -8,10 +11,55 @@ class MyGuards extends StatefulWidget {
 }
 
 class _MyGuardsState extends State<MyGuards> {
+  bool isDoneGuards = true;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('My Guards'),
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.only(left: 20, right: 20, top: 30),
+        child: Container(
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    child: CustomButton(
+                      onPressed: () => {
+                        setState(() {
+                          isDoneGuards = true;
+                        })
+                      },
+                      label: "Effectuées",
+                      buttonColor: isDoneGuards ? primaryColor : secondaryColor,
+                      textColor: isDoneGuards ? Colors.white : textColor,
+                    ),
+                  ),
+                  SizedBox(width: 20),
+                  Expanded(
+                    child: CustomButton(
+                      onPressed: () => {
+                        setState(() {
+                          isDoneGuards = false;
+                        })
+                      },
+                      label: "Demandées",
+                      buttonColor: isDoneGuards ? secondaryColor : primaryColor,
+                      textColor: isDoneGuards ? textColor : Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 10),
+              Divider(
+                color: Colors.grey,
+                height: 20,
+              ),
+              SizedBox(height: 10),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
